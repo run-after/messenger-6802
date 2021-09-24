0a97f8bba8b1accd904ad98cd66b5c7c851fe08f
run-after <run.after@protonmail.com>
2021-09-24 08:11:30 -0500

    Use bubble component to abstract out redundant code

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,63 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-end"
-  },
-  date: {
-    fontSize: 11,
-    color: "#BECCE2",
-    fontWeight: "bold",
-    marginBottom: 5
-  },
-  text: {
-    fontSize: 14,
-    color: "#91A3C0",
-    letterSpacing: -0.2,
-    padding: 8,
-    fontWeight: "bold"
-  },
-  bubble: {
-    background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
-  },
-  imgContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-end'
-  },
-  img: {
-    height: '100px',
-    margin: '0 3px',
-    borderRadius: '5px 5px 0 5px'
-  }
-}));
+import Bubble from './Bubble';
 
 const SenderBubble = (props) => {
-  const classes = useStyles();
+
   const { time, text, attachments } = props;
   return (
-    <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
-      {
-        attachments &&
-        <Box className={classes.imgContainer}>
-          {attachments.map(attachment => (
-            <img key={attachment} src={attachment} alt={attachment} className={classes.img} />
-          ))}
-        </Box>
-      }
-      {
-        text &&
-        <Box className={classes.bubble}>
-          <Typography className={classes.text}>{text}</Typography>
-        </Box>
-      }
-    </Box>
+    <Bubble time={time} text={text} attachments={attachments} />
   );
 };
 
